8bae01998111b1a4e51001128233fe36e10869c7
MasatoKato <kanperi808@gmail.com>
2024-04-20 15:00:14 +0000

    [Add] 応用課題5章 演習

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
-$(function()
+$(function ()
 {
-  $('.box1').hide();
-/*  $('.box1').css
+  $('.box1').slideDown
+  (
+    1000,
+    function ()
+    {
+      $('.box1').css
+      (
+        {
+          'background-color': '#0000FF',
+          'width': '200px',
+          'height': '100px'
+        }
+      ).slideUp(1000);
+    }
+  );
+});
+
+/*$(function()
+{
+  $('.box1').css
     (
       {
-        'background-color': '#0000FF'
+        'background-color': '#0000FF',
+        'height': '100px'
       }
-    );*/
-});
+    );
+  $('.box1').slideDown(1000);
+  
+  $('.box1').slideUp(1000);
+});*/
